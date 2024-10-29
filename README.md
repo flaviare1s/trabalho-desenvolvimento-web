e440be41c260d4b684008cfd4f47db5d82ab9994
flaviare1s <flaviarster@gmail.com>
2024-10-29 08:58:47 -0300

    Página inicial estilizada do Hotel Conforto

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# trabalho-desenvolvimento-web
-Trabalho final da cadeira de Desenvolvimento Web da Estácio
+# ATIVIDADE SUPERVISIONADA DE DESENVOLVIMENTO WEB
+PHP e MySQL
+
+## Professor: Helder Rodrigues da Costa
+
+## Instruções: 
+1.	Só serão aceitos arquivos HTML, CSS, JavaScript, PHP e MySQL.
+2.	A atividade deve ser entregue individualmente apesar de dever ser desenvolvida em grupo de até 6 alunos.
+3.	No caso e desenvolvimento em grupo devem constar no documento de cada aluno os nomes completos dos membros do grupo, senão será considerado plágio com nota ZERO para os envolvidos.
+
+## Situação problema:
+Um hotel em crescimento, quer ampliar seus negócios com a criação de uma página WEB. Uma das necessidades seria disponibilizar uma página na qual os seus possíveis hóspedes possam realizar suas reservas. O ideal seria representar uma versão digital da ficha de check in dele. Como isso é possível no ambiente web?
+
+## Orientações complementares:
+* A-	O formulário do hotel deverá ter os seguintes campos:
+•	Nome
+•	Sexo
+•	Email
+•	Data de chegada
+•	Número de noites
+•	Número de hospedes
+•	Total estimado
+•	Mensagem
+•	Newsletter
+
+* B-	O formulário do hotel deverá ser guardado em uma TABELA do MySQL, permitindo inclusão, alteração, exclusão e consulta via Página Web.
+
+
---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+const body = document.getElementById('body')
+const btnDark = document.getElementById('btn-sol')
+const btnLight = document.getElementById('btn-lua')
+const titleLight = document.getElementById('title-light')
+const titleDark = document.getElementById('title-dark')
+const imagemLight = document.getElementById('light-imagem')
+const imagemDark = document.getElementById('dark-imagem')
+const sobre = document.getElementById('sobre')
+const footer = document.getElementById('footer')
+
+btnLight.addEventListener('click', () => {
+  btnLight.classList.add('hidden')
+  btnDark.classList.remove('hidden')
+  body.style.backgroundColor = '#000000'
+  titleLight.classList.add('hidden')
+  titleDark.classList.remove('hidden')
+  imagemLight.classList.add('hidden')
+  imagemDark.classList.remove('hidden')
+  footer.classList.remove('footer-light')
+  sobre.classList.remove('text-black')
+  sobre.classList.add('text-white')
+})
+
+btnDark.addEventListener('click', () => {
+  btnDark.classList.add('hidden')
+  btnLight.classList.remove('hidden')
+  body.style.backgroundColor = '#ffffff'
+  titleDark.classList.add('hidden')
+  titleLight.classList.remove('hidden')
+  imagemDark.classList.add('hidden')
+  imagemLight.classList.remove('hidden')
+  footer.classList.add('footer-light')
+  sobre.classList.remove('text-white')
+  sobre.classList.add('text-black')
+})

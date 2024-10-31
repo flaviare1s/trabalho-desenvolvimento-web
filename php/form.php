875911dde0dbb6e4b919aa172b45c3b751a2fb5c
RZRPOLARIS45 <moreirafelipe090@gmail.com>
2024-10-31 16:07:39 -0300

    Realizada a criação de classes no arquivo form.php e uma estilização pelo arquivo form.css.

--- php/form.php
+++ php/form.php
@@ -8,46 +8,40 @@
 </head>
 <body>
     <h1>Faça a sua reserva:</h1>
-    <form method="post"action="reserva.php" >
+    <form method="post" action="reserva.php">
+        <label class="label-text" for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required><br>
 
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome"required><br><br>
+        <label class="label-text" for="sexo">Sexo:</label>
+        <select id="sexo" name="sexo" required>
+            <option value=""></option>
+            <option value="ac">Masculino</option>
+            <option value="al">Feminino</option>
+        </select><br>
 
-        <label for="sexo">Sexo:</label>
-     <select id="sexo" name="sexo"required>
-    <option value=""></option>
-    <option value="ac">Masculino</option>
-    <option value="al">Feminino</option>
-    </select><br><br>
-    
+        <label class="label-text" for="email">E-mail:</label>
+        <input type="text" id="email" name="email" required><br>
 
-        <label for="email">E-mail:</label>
-        <input type="text" id="email" name="email" required><br><br>
+        <label class="label-text" for="data_de_chegada">Data de Chegada:</label>
+        <input type="text" id="data_de_chegada" name="data_de_chegada" required><br>
 
-        <label for="data_de_chegada">Data de Chegada:</label>
-        <input type="text" id="data_de_chegada" name="data_de_chegada" required><br><br>
+        <label class="label-text" for="numero_de_noites">Número de noites:</label>
+        <input type="text" id="numero_de_noites" name="numero_de_noites" required><br>
 
-        <label for="numero_de_noites">Número de noites:</label>
-        <input type="text" id="numero_de_noites" name="numero_de_noites" required><br><br>
+        <label class="label-text" for="numero_de_hospedes">Número de hospedes:</label>
+        <input type="number" min="1" max="99" id="numero_de_hospedes" name="numero_de_hospedes" required><br>
 
-        <label for="numero_de_hospedes">Número de hospedes:</label>
-        <input type="number" min="1" max="99" id="numero_de_hospedes" name="numero_de_hospedes" required><br><br>
+        <label class="label-text" for="total_estimado">Total estimado:</label>
+        <input type="text" id="total_estimado" name="total_estimado" required><br>
 
-         <label for="total_estimado">Total estimado:</label>
-        <input type="text" id="total_estimado" name="total_estimado" required><br><br>
+        <label class="label-text" for="mensagem">Mensagem ou Observaçoes:</label><br>
+        <textarea id="mensagem" name="mensagem"></textarea><br>
 
-        <label for="mensagem">Mensagem ou Observaçoes:</label><br>
-        <textarea id="mensagem" name="mensagem"></textarea><br><br>
-
-        <label for="newsletter">Gostaria de receber e-mails contendo atualizações, promoções entre outras coisas ?</label> <br><br>
-        <input type="radio"  id="newsletter" name="newsletter">Sim  
-        <input type="radio"  id="newsletter" name="newsletter" checked="checked">Não <br><br> 
-
-
-        
+        <label class="label-text" for="newsletter">Gostaria de receber e-mails contendo atualizações, promoções entre outras coisas?</label><br>
+        <input type="radio" id="newsletter" name="newsletter" value="sim">Sim
+        <input type="radio" id="newsletter" name="newsletter" value="nao" checked="checked">Não<br>
 
         <input type="submit" value="Confirmar">
     </form>
-
 </body>
 </html>

cef3debfe44e66b687bb48686da523337b23b812
flaviare1s <flaviarster@gmail.com>
2024-11-03 11:43:47 -0300

    Ajusta página de success

--- php/success.php
+++ php/success.php
@@ -4,13 +4,15 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/success.css">
   <title>Reserva Confirmada</title>
 </head>
 
 <body>
   <h1>Reserva Confirmada!</h1>
   <p>Obrigado pela sua reserva. Você receberá um e-mail com os detalhes em breve.</p>
-  <a href="../index.html">Voltar para a Home</a>
+  <a style="display: block; text-align: center; text-decoration: none; font-weight: bold; color: var(--white); margin-bottom: 10px; width: 100%; padding: 10px;"
+    href="../index.html">Voltar para a Home</a>
 </body>
 
 </html>
